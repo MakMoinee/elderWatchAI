257e0f7f7c947bf32dd594da54084f51be32c19e
Mak Moinee <titus.silver.ph@gmail.com>
2023-12-23 16:23:13 +0800

    update code

--- api.py
+++ api.py
@@ -1,25 +1,25 @@
-from flask import Flask, jsonify, request
+from quart import Quart, jsonify, request
 from scapy.all import *
 import firebase_admin
 from firebase_admin import credentials, firestore
+import cv2
+import asyncio
 
-app = Flask(__name__)
+app = Quart(__name__)
 
 # Initialize Firebase Admin SDK
 cred = credentials.Certificate('./elderwatch.json')  # Replace with your service account key file
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
-def is_camera(ip_address):
-    packet = IP(dst=ip_address) / TCP(dport=554, flags='S')  # RTSP port 554 used by many cameras
-
-    # Sending packet and waiting for response
-    response = sr1(packet, timeout=2, verbose=0)
-
-    if response is not None and response.haslayer(TCP):
-        if response[TCP].flags == 18:  # Checking for a TCP response indicating an open port
+def is_rtsp_accessible(rtsp_url):
+    try:
+        cap = cv2.VideoCapture(rtsp_url)
+        if cap.isOpened():
+            cap.release()
             return True
-
+    except Exception as e:
+        print(f"Error: {e}")
     return False
 
 # Endpoint to retrieve users
@@ -31,19 +31,20 @@ def get_users():
 
 
 @app.route("/ping", methods=['GET'])
-def ping_ip():
+async def ping_ip():
     ip = request.args.get('ip')  # Get the 'ip' parameter from the request query string
     if not ip:
         return jsonify({"message": "IP address is missing in the request parameters"}), 400
 
-    print(f"Pinging IP: {ip}")
+    rtsp_url = f"rtsp://{ip}/live/ch00_0"
 
-    is_camera_ip = is_camera(ip)
+    loop = asyncio.get_event_loop()
+    result = await loop.run_in_executor(None, is_rtsp_accessible, rtsp_url)
 
-    if is_camera_ip:
-        return jsonify({"message": f"The IP address {ip} is associated with a camera."}), 200
+    if result:
+        return jsonify({"ip": ip, "status": "RTSP URL is accessible and working"}), 200
     else:
-        return jsonify({"message": f"The IP address {ip} may not be a camera or is not responding on the RTSP port."}), 200
+        return jsonify({"ip": ip, "status": "RTSP URL may not be accessible or is not working"}), 200
 
 
 if __name__ == '__main__':
